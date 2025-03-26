7b3328d58622f4c64cc59e64a35ad09a318f360f
namnt1304 <namnt1304@gmail.com>
2025-03-26 20:47:07 +0900

    55 test commit 1

---
+++ test1.ts
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class test1 extends Component {
+    render() {
+        return (
+            <div>
+                123
+            </div>
+        );
+    }
+}
+
+export default test1;
