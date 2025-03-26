b8b6b8e09234a39a033b0d43c812c76ced029fd3
namnt1304 <namnt1304@gmail.com>
2025-03-26 20:48:10 +0900

    #55 test commit 2

---
+++ test2.ts
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class test2 extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+test2.propTypes = {
+
+};
+
+export default test2;
