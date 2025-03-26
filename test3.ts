658a969320930901d85e95f9ea967efd55535482
namnt1304 <namnt1304@gmail.com>
2025-03-26 20:49:17 +0900

    #1 test commit 1

---
+++ test3.ts
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class test3 extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentWillMount() {
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+test3.propTypes = {
+
+};
+
+export default test3;
