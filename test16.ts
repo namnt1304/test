b31c1aa5bdcc51e98677f7b62e8f09d597144af3
namnt0413 <namnt0413@gmail.com>
2025-03-26 21:04:18 +0900

    #1 test commit 16

---
+++ test16.ts
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export class test16 extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+      <div>test16</div>
+    )
+  }
+}
+
+export default test16
